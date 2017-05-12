6cc2ab744b35940c796f9e8b562ad4da20f40ddf
Olykir <oillicker806@gmail.com>
2017-05-12 01:10:34 -0500

    Update art.js

--- commands/art.js
+++ commands/art.js
@@ -1,28 +1,19 @@
 const figlet = require('figlet');
-
 exports.run = (bot, msg, args) => {
     if (args.length < 1) {
          msg.channel.send("Please put some text after the command"); return // This makes sure there is text in the command
     }
-
-
-
     figlet.text(args.join(' '), (err, res) => {
         if (err)
             return msg.error(err);
         msg.channel.send({embed: {description: `\`\`\`${res}\`\`\``, color: 14814143}});
-    }); // This is what makes the art NOTE: You need figlet
-
-};
-
-
+    });};
 exports.conf = {
   enabled: true,
   guildOnly: true,
   aliases: ['figlet'],
   permLevel: 0
 };
-
 exports.help = {
   name: 'art',
   description: 'Displays Ascii text art.',
