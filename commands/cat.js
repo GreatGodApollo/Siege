5ca490ca5af1c22f30ef97cb8dc8fd9b6cf77c35
Olykir <oillicker806@gmail.com>
2017-05-12 01:14:19 -0500

    Seems like its a good idea, right?

---
+++ commands/cat.js
@@ -0,0 +1,20 @@
+
+
+exports.run = (client, msg, [num]) => {
+msg.channel.send({ files: ["./images/cat2.png"] }) // sends a picture of a cat
+  }
+
+
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'cat',
+  description: 'Find your cat.',
+  usage: 'cat'
+};
