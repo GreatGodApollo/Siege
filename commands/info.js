f335a224a5919d317283a9d1e6f307857f749035
Olykir <oillicker806@gmail.com>
2017-05-12 00:26:39 -0500

    Info Command

---
+++ commands/info.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const moment = require("moment");
+require("moment-duration-format");
+exports.run = (client, message) => {
+
+
+  const embed = new Discord.RichEmbed()
+    .setAuthor('Siege Info')
+
+
+
+    .setColor(14814143)
+    .setFooter('Bot Info')
+    .setThumbnail(client.avatarURL)
+
+
+      .addField(' ❯ Memory Usage', `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, false)
+
+    .addField(' ❯ Uptime',` ${client.uptime} MS`, true)
+
+        .addField(`❯ Version`, `v${Discord.version}`, false)
+.addField(`❯ Creators`, `[@єรςคթє#4896](thehangout.tk)\n@ʍɛɨֆȶɛʀ#7070\n[@Apollo (beezTEM)#9746](https://www.youtube.com/channel/UC2aexUupMf_UzoEArAXnGDA)\n[@Olykir#2588](https://www.reddit.com/u/Olykir)`, true)
+
+
+  message.channel.send({embed})
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'info',
+  description: `Displays the bot info`,
+  usage: 'info'
+};
